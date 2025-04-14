73992a75205c8b8ab7be0385469e538e946ffdbc
Manuel Cattelan <cattelanmanuel01@gmail.com>
2025-04-14 08:55:52 +0200

    feat: complete scanner implementation for parsing tokens

--- compiler.c
+++ compiler.c
@@ -1,4 +1,22 @@
 #include "compiler.h"
 #include "scanner.h"
+#include <stdio.h>
 
-void compile(const char *input) { init_scanner(input); }
+void compile(const char *input) {
+  init_scanner(input);
+
+  int line = -1;
+  for (;;) {
+    Token current_token = scan_token();
+    if (current_token.line_number != line) {
+      printf("%4zu ", current_token.line_number);
+      line = current_token.line_number;
+    } else {
+      printf("   | ");
+    }
+    printf("%2d '%.*s'\n", current_token.type, (int)current_token.lexeme_length,
+           current_token.lexeme_start);
+    if (current_token.type == TOKEN_EOF)
+      break;
+  }
+}
--- scanner.h
+++ scanner.h
@@ -3,6 +3,49 @@
 
 #include "common.h"
 
+typedef enum {
+  TOKEN_LEFT_PARENTHESIS,
+  TOKEN_RIGHT_PARENTHESIS,
+  TOKEN_LEFT_BRACE,
+  TOKEN_RIGHT_BRACE,
+  TOKEN_DOT,
+  TOKEN_COMMA,
+  TOKEN_SEMICOLON,
+  TOKEN_PLUS,
+  TOKEN_MINUS,
+  TOKEN_SLASH,
+  TOKEN_STAR,
+  TOKEN_BANG,
+  TOKEN_BANG_EQUAL,
+  TOKEN_EQUAL,
+  TOKEN_EQUAL_EQUAL,
+  TOKEN_GREATER,
+  TOKEN_GREATER_EQUAL,
+  TOKEN_LESS,
+  TOKEN_LESS_EQUAL,
+  TOKEN_IDENTIFIER,
+  TOKEN_STRING,
+  TOKEN_NUMBER,
+  TOKEN_VAR,
+  TOKEN_NIL,
+  TOKEN_AND,
+  TOKEN_OR,
+  TOKEN_IF,
+  TOKEN_ELSE,
+  TOKEN_TRUE,
+  TOKEN_FALSE,
+  TOKEN_FOR,
+  TOKEN_WHILE,
+  TOKEN_CLASS,
+  TOKEN_SUPER,
+  TOKEN_THIS,
+  TOKEN_FUN,
+  TOKEN_PRINT,
+  TOKEN_RETURN,
+  TOKEN_ERROR,
+  TOKEN_EOF
+} TokenType;
+
 /* Our scanner object keeps track of how far it has gone through the user's
  * source code by using a counter, "line_number", which tracks what line the
  * current lexeme is on, and two pointers, "lexeme_start" and "lexeme_current",
@@ -14,6 +57,13 @@ typedef struct {
   size_t line_number;
 } Scanner;
 
+typedef struct {
+  TokenType type;
+  const char *lexeme_start;
+  size_t lexeme_length;
+  size_t line_number;
+} Token;
+
 /*
  * @brief Initialize our global scanner.
  * This function will set the scanner's "lexeme_start" and "lexeme_current" to
@@ -23,5 +73,14 @@ typedef struct {
  * @return void
  */
 void init_scanner(const char *input);
+
+/*
+ * @brief Scan the next token from the source code.
+ * This function will scan the next token from the source code and return it to
+ * the caller.
+ *
+ * @return The token that was just scanned
+ */
+Token scan_token();
 
 #endif
